305ae65db0cc67ab2c3110159ff0195ec1517db0
unknown <neduvinci@gmail.com>
2019-11-01 12:31:47 +0100

    Added endpoint to remove action by id

--- router/actionRouter.js
+++ router/actionRouter.js
@@ -1,16 +1,34 @@
 const express = require('express')
+const actionDb = require('../data/helpers/actionModel')
 const {
     validateProjectList,
     validateProjectId,
     validateActionList,
     validateActionId
 } = require('../middleware')
-//todo get(id), insert(action), update(id, changes), remove(id)
+//todo insert(action), update(id, changes), remove(id)
 const router = express.Router()
 const actionGetValidation = [validateProjectList, validateProjectId, validateActionList, validateActionId]
 
 router.get('/:id/actions/:action_id', actionGetValidation, (req, res, next) => {
     res.status(200).json(req.action)
+})
+
+router.delete('/:id/actions/:action_id', actionGetValidation, (req, res, next) => {
+    const { action_id } = req.params
+    actionDb.remove(action_id)
+        .then(data => {
+            res
+                .status(200)
+                .json({
+                    message: "The action of id " + action_id + " was successfully removed."
+                })
+        })
+        .catch(error => {
+            next({
+                message: "There was an error in the removal of action id " + action_id + " from the database. Please check the action list" + error.message
+            })
+        })
 })
 
 module.exports = router
