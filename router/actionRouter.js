274bb80aa7ebbbd58d8a2591ae8843771c26471a
unknown <neduvinci@gmail.com>
2019-11-01 12:16:45 +0100

    Added endpoint to get action by id

--- router/actionRouter.js
+++ router/actionRouter.js
@@ -1,9 +1,16 @@
 const express = require('express')
-
+const {
+    validateProjectList,
+    validateProjectId,
+    validateActionList,
+    validateActionId
+} = require('../middleware')
+//todo get(id), insert(action), update(id, changes), remove(id)
 const router = express.Router()
+const actionGetValidation = [validateProjectList, validateProjectId, validateActionList, validateActionId]
 
-router.get('/', (req, res, next) => {
-    res.json("Hello from the action jungle")
+router.get('/:id/actions/:action_id', actionGetValidation, (req, res, next) => {
+    res.status(200).json(req.action)
 })
 
 module.exports = router
