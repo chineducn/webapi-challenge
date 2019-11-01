87f6ad451d0cfa98c9cfa16f598e0be2d5f132ea
unknown <neduvinci@gmail.com>
2019-11-01 11:19:36 +0100

    Added endpoints to get project by id and delete a project

--- router/projectRouter.js
+++ router/projectRouter.js
@@ -2,7 +2,8 @@ const express = require('express')
 const projectDb = require('../data/helpers/projectModel')
 const {
     validateProjectList,
-    validateProject
+    validateProject,
+    validateProjectId,
 } = require('../middleware')
 
 const router = express.Router()
@@ -11,6 +12,29 @@ router.get('/', validateProjectList, (req, res, next) => {
     res
         .status(200)
         .json(req.projects)
+})
+
+router.get('/:id', validateProjectList, validateProjectId, (req, res, next) => {
+    res
+        .status(200)
+        .json(req.project)
+})
+
+router.delete('/:id', validateProjectList, validateProjectId, (req, res, next) => {
+    const { id } = req.params
+    projectDb.remove(id)
+        .then(data => {
+            res
+                .status(200)
+                .json({
+                    message: "The project of id " + id + " was successfully removed."
+                })
+        })
+        .catch(error => {
+            next({
+                message: "There was an error in the removal of project id " + id + " from the database. Please check the project list" + error.message
+            })
+        })
 })
 
 router.post('/', validateProject, (req, res, next) => {
