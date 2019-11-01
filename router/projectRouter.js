91064b646d07ee9d7cd9d5b4dea3097b774aad90
unknown <neduvinci@gmail.com>
2019-11-01 10:50:39 +0100

    added and tested endpoint for creating new project

--- router/projectRouter.js
+++ router/projectRouter.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const projectDb = require('../data/helpers/projectModel')
-const { validateProjectList } = require('../middleware')
+const {
+    validateProjectList,
+    validateProject
+} = require('../middleware')
 
 const router = express.Router()
 
@@ -8,6 +11,20 @@ router.get('/', validateProjectList, (req, res, next) => {
     res
         .status(200)
         .json(req.projects)
+})
+
+router.post('/', validateProject, (req, res, next) => {
+    projectDb.insert(req.body)
+        .then(newProject => {
+            res
+                .status(201)
+                .json(newProject)
+        })
+        .catch(error => {
+            next({
+                message: "There was an error in the creation of the new project. Check project list for confirmation" + error.message
+            })
+        })
 })
 
 module.exports = router
